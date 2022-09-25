aee784f5642ae21863d1d8087a9c47588808449b
jcuelc00 <114149958+jcuelc00@users.noreply.github.com>
2022-09-25 17:30:32 +0200

    Add files via upload

---
+++ js/firebase_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
+    databaseURL: process.env.DATABASEURL
+};
